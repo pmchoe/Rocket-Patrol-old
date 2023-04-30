c15943813018abc5802f96d1cee4eda07f23b418
pmchoe <130533114+pmchoe@users.noreply.github.com>
2023-04-29 21:50:25 -0700

    Update Spaceship.js

--- Rocket Patrol/src/scenes/prefabs/Spaceship.js
+++ Rocket Patrol/src/scenes/prefabs/Spaceship.js
@@ -1,6 +1,24 @@
 // Spaceship prefab
 class Spaceship extends Phaser.GameObjects.Sprite {
     constructor(scene, x, y, texture, frame, pointValue) {
-        super(scene, )
+        super(scene, x, y, texture, frame);
+        scene.add.existing(this);
+        this.points = pointValue;
+        this.moveSpeed = game.settings.spaceshipSpeed;
+    }
+
+    update() {
+        // move spaceship left
+        this.x -= this.moveSpeed;
+        
+        // wrap around from left edge to right edge
+        if(this.x <= 0 - this.width) {
+            this.reset();
+        }
+    }
+
+    // position reset
+    reset() {
+        this.x = game.config.width;
     }
 }
